086c1715ed28c73b64c42d54bc58981e2bc756d7
Emeka Augustine <cafafanscoders@gamil.com>
2019-02-02 02:54:43 -0500

    Product Resource created

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class Review extends Model
 {
-    //
+    //Review belongs to a product
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
+
 }
